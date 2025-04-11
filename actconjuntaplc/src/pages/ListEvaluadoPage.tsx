7e651248ea17a2e2d2639cfbd93614b0e33b7fa8
libiaf <a01253425@tec.mx>
2025-04-11 16:58:18 -0700

    Crear header

--- actconjuntaplc/src/pages/ListEvaluadoPage.tsx
+++ actconjuntaplc/src/pages/ListEvaluadoPage.tsx
@@ -1,14 +1,15 @@
-import Filter from "./Filter";
-import List from "./List";
+import Filter from "../components/Filter";
+import List from "../components/List";
+import Header from "../components/Header";
 import { Evaluado } from "my-types";
 import { useState, useEffect } from "react";
 import { deleteEvaluado, getAllEvaluados } from "../api/EvaluadoAPI";
 
 export default function ListPerson() {
 
-  const[name, setName] = useState<string>("")
-  const[graduado, setGraduado] = useState<string>("All")
-  const[evaluados, setEvaluados] = useState<Evaluado[]>([]);
+  const [name, setName] = useState<string>("");
+  const [graduado, setGraduado] = useState<string>("All");
+  const [evaluados, setEvaluados] = useState<Evaluado[]>([]);
 
   const handleDelete = async (id: number) => {
     try {
@@ -21,25 +22,32 @@ export default function ListPerson() {
 
   const filteredProducts = evaluados.filter((evaluado) => {
     return (
-        (graduado === "All" || evaluado.graduado === graduado) &&
-        (evaluado.nombre.toLowerCase().includes(name.toLowerCase()))
+      (graduado === "All" || evaluado.graduado === graduado) &&
+      evaluado.nombre.toLowerCase().includes(name.toLowerCase())
     );
   });
 
   useEffect(() => {
     getAllEvaluados().then((data: Evaluado[]) => setEvaluados(data));
   }, []);
-  console.log(evaluados);
-
+  
   return (
     <>
-      <div className="flex flex-col gap-4 my-4 ">
+      <Header />
+
+      <div className="flex flex-col gap-4 my-4 px-6">
         <h3 className="text-3xl font-bold text-gray-800 text-left">
           Lista de evaluados
         </h3>
+
         <div className="h-4"></div>
 
-        <Filter graduado={graduado} setGraduado={setGraduado} name={name} setName={setName}/>
+        <Filter
+          graduado={graduado}
+          setGraduado={setGraduado}
+          name={name}
+          setName={setName}
+        />
 
         <div className="flex justify-end items-center">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -47,13 +55,10 @@ export default function ListPerson() {
           </button>
         </div>
       </div>
-      <div className="relative overflow-x-auto">
-        <List evaluados={filteredProducts} onDelete={handleDelete}/>
+
+      <div className="relative overflow-x-auto px-6 pb-10">
+        <List evaluados={filteredProducts} onDelete={handleDelete} />
       </div>
-      {/* <div>
-        <Filter />
-        <List />
-      </div> */}
     </>
   );
 }
